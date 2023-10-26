8eed8d15e1ae0da9c278bf9f1ba5efba04ebae4a
RHGDEV <21209674+RHGDEV@users.noreply.github.com>
2023-10-26 12:38:04 -0400

    y/n statement fix / debugging

--- maint/scripts/accountcreate.sh
+++ maint/scripts/accountcreate.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# VALUES
-local blockquota = 500000000 # 500Gb soft block limit
-local blocklimit = 500000000 # 500Gb hard block limit
-local inodequota = 0 # inf soft inode limit
-local inodelimit = 0 # inf hard inode limit
-
 read -p "Enter username : " username # Get username from user
 read -s -p "Enter password : " password # Get password from user
 egrep "^$username" /etc/passwd >/dev/null # Check if user exists
@@ -13,17 +7,20 @@ if [ $? -eq 0 ]; then # If user exists
 	echo "${username} exists!"
 	exit 1
 fi
-read -p "Make user a sudoer? [y/N] " user_sudo # Ask if user should be sudoer
+echo "" # New line
+read -p "Make user a sudoer? [Y/n] " user_sudo # Ask if user should be sudoer
 read -p "Set quotas for user? [Y/n] " user_quotas # Ask if user should have quotas
+echo username:$username password:$password sudo:$user_sudo quotas:$user_quotas # Debug
+sudo useradd -m -p "$password" "$username" # Create user with password and home directory
 
-sudo useradd -m -p "$password" "$username" # Create user
-
-if [ $user_sudo ]; then # If user should be sudoer
+if [[ "$user_sudo" =~ [Nn] ]]; then # If user should be sudoer
 	sudo usermod -aG wheel $username
 fi
 
-if [ $user_quotas ]; then # If user should have quotas
-	sudo setquota -u $username $blockquota $blocklimit $inodequota $inodelimit /home
+# 500Gb soft/hard block limit
+# inf soft/hard inode limit
+if [[ "$user_sudo" =~ [Nn] ]]; then # If user should have quotas
+	sudo setquota -u $username 500000000 500000000 0 0 /home
 fi
 
 # Configure Httpd public html directories
