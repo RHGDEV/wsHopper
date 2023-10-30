58f09b487ad476f629b68b40545650f5e62cac9a
RHGDEV <21209674+RHGDEV@users.noreply.github.com>
2023-10-30 11:15:13 -0400

    full upgrade to menu selection

--- lib/lib.sh
+++ lib/lib.sh
@@ -110,14 +110,10 @@ askformaint() {
 
 # Ask if the installer wants to perform a full system upgrade before continuing
 performupgrades() {
-	if askbool "Perform a full upgrade now?"; then
-		info "Performing a full system upgrade. This may take a while..."
-		sudo dnf upgrade -y
-		success "System upgrade complete. Awaiting for any keypress to continue..."
-		read
-	else
-		output "Skipping system upgrade."
-	fi
+	info "Performing a full system upgrade. This may take a while..."
+	sudo dnf upgrade -y
+	success "System upgrade complete. Awaiting for any keypress to continue..."
+	read
 }
 
 # Helper function
