e335cf4bafd663c60ac34c70267b3da53a590f3d
RHGDEV <21209674+RHGDEV@users.noreply.github.com>
2023-11-28 09:57:38 -0500

    remove sudo question

--- maint/scripts/accountcreate.sh
+++ maint/scripts/accountcreate.sh
@@ -8,14 +8,9 @@ if [ $? -eq 0 ]; then # If user exists
 	exit 1
 fi
 echo "" # New line
-read -p "Make user a sudoer? [Y/n] " user_sudo # Ask if user should be sudoer
 read -p "Set quotas for user? [Y/n] " user_quotas # Ask if user should have quotas
 echo username:$username password:$password sudo:$user_sudo quotas:$user_quotas # Debug
 sudo useradd -m -p "$password" "$username" # Create user with password and home directory
-
-if [[ "$user_sudo" =~ [Nn] ]]; then # If user should be sudoer
-	sudo usermod -aG wheel $username
-fi
 
 # 500Gb soft/hard block limit
 # inf soft/hard inode limit
